a93b5ed15d8a632427e976c6a5b79322e3269c17
Suad shire <jido@abdulazizs-MacBook-Air.local>
2020-02-26 21:41:00 +0300

    firfst commit

---
+++ micasa/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Hood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
